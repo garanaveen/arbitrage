2023de87694d0625d98a723fe2f0a1f44f783f1f
Logmein Gn <logmeingn@Logmeins-MacBook-Pro.local>
2018-02-22 23:12:32 -0600

    Getting info directly from url

--- arbitrage.py
+++ arbitrage.py
@@ -1,44 +1,44 @@
 #!/usr/bin/env python
 
-
 import json
+import shutil
+import wget
+import os
+
+koinex_url="https://koinex.in/api/ticker"
+gdax_btc_url = "https://api.gdax.com/products/BTC-USD/ticker"
+currency_url="https://api.fixer.io/latest?base=USD"
+
+def readurl(url,outputFile = "ticker"):
+	output = outputFile
+	if os.path.exists(output):
+	   os.remove(output)
+
+	file = wget.download(url,output)
+	f = open(file, 'r')
+	htmlText = "\n".join(f.readlines())
+	f.close()
+
+	return json.loads(htmlText)
 
 def readfile(filename):
    filecontents = ""
-   with open("usdinr.txt", "r") as myfile:
+   with open(filename, "r") as myfile:
       filecontents = myfile.read()
    return json.loads(filecontents)
 
-
-def get_usd_inr_conversionusingreadfile():
-   jsonfile = readfile("usdinr.txt")
+def get_usd_inr_conversion():
+   jsonfile = readurl(currency_url, "usdinr.json")
    return jsonfile['rates']['INR']
 
-
-def get_usd_inr_conversion():
-   filecontents = ""
-   with open("usdinr.txt", "r") as myfile:
-      filecontents = myfile.read()
-   xchangeratejson = json.loads(filecontents)
-
-   return xchangeratejson['rates']['INR']
-   
-   
-def get_price_koinex(currency, filename):
-   filecontents = ""
-   with open(filename, "r") as myfile:
-      filecontents = myfile.read()
-   koinexjson = json.loads(filecontents)
-   price = koinexjson['prices'][currency]
-   return  price
+def get_price_koinex(currency):
+   jsonfile = readurl(koinex_url, "koinex.json")
+   return jsonfile['prices'][currency]
 
 #Redundant method. Create a class and override getting the price part.
-def get_price_gdax(currency, filename):
-   filecontents = ""
-   with open(filename, "r") as myfile:
-      filecontents = myfile.read()
-   gdaxjson = json.loads(filecontents)
-   price = gdaxjson['price']
+def get_price_gdax(currency, filename): #currency is unused for now.
+   jsonfile = readurl(gdax_btc_url, "gdax_btc.json")
+   price = jsonfile['price']
    return  price
 
 def calculate_arbitrage(usdprice, inrprice, conversionrate):
@@ -48,11 +48,11 @@ def calculate_arbitrage(usdprice, inrprice, conversionrate):
    
 
 if __name__ == "__main__":
-   price_koinex =  float(get_price_koinex("BTC", "koinexprice.txt"))
+   price_koinex =  float(get_price_koinex("BTC"))
    print("koinex btc : " + str(price_koinex))
    price_gdax = float(get_price_gdax("BTC", "gdax.txt"))
    print("gdax btc : " + str(price_gdax))
-   usd_inr = get_usd_inr_conversionusingreadfile()
+   usd_inr = get_usd_inr_conversion()
    print("usd-inr : " + str(usd_inr))
    print("arbitrage : " + str(calculate_arbitrage(price_gdax, price_koinex, usd_inr))) 
 
