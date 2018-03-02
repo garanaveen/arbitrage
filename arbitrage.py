d17acd91848719d3a0c21a1af62682e6a9780202
logmeingn <logmeingn@gmail.com>
2018-03-01 22:05:57 -0600

    logging with timestamp

--- arbitrage.py
+++ arbitrage.py
@@ -10,6 +10,15 @@ class Arbitrage:
    def __init__(self, exchange1, exchange2):
       self.e1 = exchange1
       self.e2 = exchange2
+      
+      #TODO : Extract this logger to a common place so that other files can use the logging.
+      self.logger = logging.getLogger('spam_application')
+      self.logger.setLevel(logging.DEBUG)
+      fh = logging.FileHandler('history_arbitrage.log')
+      formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+      fh.setFormatter(formatter)
+      self.logger.addHandler(fh)
+
    
    def printarbitrage(self):
       self.calculate_arbitrage("btc", self.e1.price.btc, self.e2.price.btc)
@@ -19,6 +28,6 @@ class Arbitrage:
 
    def calculate_arbitrage(self, currency, p1, p2):
       ratio = (p1-p2)*100/p1
-      logging.info(currency + "-" + str(self.e1) + ":" + str(p1) + "," + str(self.e2) + ":" + "," + str(p2) + ", ratio:" + str(ratio))
+      self.logger.info(currency + "-" + str(self.e1) + ":" + str(p1) + "," + str(self.e2) + ":" + "," + str(p2) + ", ratio:" + str(ratio))
       
 
--- projectconfig.py
+++ projectconfig.py
@@ -2,7 +2,10 @@
 
 import logging
 
-logging.basicConfig(filename="history_arbitrage.log", level=logging.INFO)
+#FORMAT = "%(asctime)-15s %(clientip)s %(user)-8s %(message)s"
+#logging.basicConfig(filename="history_arbitrage.log")
+#logging.basicConfig(level=logging.INFO)
+#logging.basicConfig(format=FORMAT, filename="history_arbitrage.log")
 
 
 livequote = False
