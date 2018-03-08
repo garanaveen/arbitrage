b0196cd1faee363343224834dc6d4a6a3f8e5655
garanaveen <garanaveen@gmail.com>
2018-03-08 06:00:52 -0600

    Updated with email sending bookmarks.

--- testsendgmail.py
+++ testsendgmail.py
@@ -8,3 +8,12 @@ server.starttls()
 server.login('testgmailaccount@gmail.com','xxxxxxxx')
 problems = server.sendmail('testgmailaccount@gmail.com', to_addr_list, message)
 server.quit()
+
+
+
+
+#Bookmarks,
+#https://developers.google.com/gmail/api/guides/sending
+
+#For sending emails programmaticaly,
+#https://login.mailchimp.com/signup/success/ 
