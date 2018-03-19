a9013ab8033c683b0c2d7073cd1464bc6405cb43
Naveen <garanaveen@gmail.com>
2018-03-19 10:12:37 -0500

    Added myalertsettings.py so that user can add entries which match the criteria to get notified.

---
+++ myalertsettings.py
@@ -0,0 +1,11 @@
+
+#User should be able to get notifications by adding entries here.
+
+
+#If arbitrage for buy price of LTC is lessthan 3%, notify me.
+MyAlerts['ltc']['buy']['lessthan'] = 3
+
+
+#If arbitrage for sell price of ETH is greaterthan 9%, notify me.
+MyAlerts['eth']['sell']['greaterthan'] = 9
+
