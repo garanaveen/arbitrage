c48c86d4009f02f9d8865571bc14ad6a230100da
Naveen <garanaveen@gmail.com>
2018-03-27 15:49:08 -0500

    parsing of myalerts.ini

---
+++ alertsettings.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import json
+import shutil
+import wget
+import os
+import projectconfig as cfg
+
+
+FILE_NAME = "myalerts.ini"
+
+class AlertSettings:
+   alerts = []
+
+
+   def parse_my_alert_settings(self):
+      print("parse")
+
+      f = open(FILE_NAME, 'r')
+      myalert = "dummyline"
+      #TODO : If its not "LTC<2" format, ignore that line.
+      while myalert:
+          myalert = f.readline()
+          if '>' in myalert:
+             transactionType = "sell"
+             currency,ratio = myalert.split('>')
+          elif '<' in myalert:
+             transactionType = "buy"
+             currency,ratio = myalert.split('<')
+          else:
+             #ignore this line
+             continue
+
+          print "currency : " + currency.lower() + ", transactionType : " + transactionType + ", ratio : " + ratio 
+
+      f.close()
+
+if __name__ == "__main__":
+   alrtSettings = AlertSettings()
+   alrtSettings.parse_my_alert_settings()
+
