a26f7a083226fba3f3a1ab3983fe064f58f389d5
Naveen Gara <garanaveen@johndeere.com>
2018-04-26 10:17:29 -0500

    Using fixer.io api for getting the exchange rate.

--- exchangerate.py
+++ exchangerate.py
@@ -3,7 +3,8 @@
 from utils import readurl
 import projectconfig as cfg
 
-url="https://api.fixer.io/latest?base=USD"
+#url="https://api.fixer.io/latest?base=USD"
+url="http://data.fixer.io/api/latest?access_key=075191d834b8ceef6b09ea55dfa4b127"
 
 def is_correct_frequency():
    return cfg.ITERATION % cfg.EXCHANGERATE_FREQUENCY == 0
@@ -11,24 +12,33 @@ def is_correct_frequency():
 def is_correct_platform():
    #Linux platform has some trouble doing a wget on this url. If its linux, then execute this command and then run "./arbitrage".
    #wget https://api.fixer.io/latest?base=USD -O .exchangerate.json
-   return cfg.PLATFORMTYPE != "linux"
+   #return cfg.PLATFORMTYPE != "linux"
+   return True
+
+def get_usdinr():
+      jsonfile = readurl(url, ".exchangerate.json")
+      inr = float(jsonfile['rates']['INR'])
+      usd = float(jsonfile['rates']['USD'])
+      exchangerate = inr/usd
+      print "inr :" + str(inr)
+      print "usd :" + str(usd)
+      print "exchangerate :" + str(exchangerate)
+      return exchangerate
 
 def get_exchangerate():
    if is_correct_frequency() and is_correct_platform():
       print "Getting live exchange rate"
-      jsonfile = readurl(url, ".exchangerate.json")
-      exchangerate = float(jsonfile['rates']['INR'])
+      exchangerate = get_usdinr()
    else:
+      print "Not getting live exchange rate"
       liveQuoteOriginalValue = cfg.LIVEQUOTE
       cfg.LIVEQUOTE = False
-      jsonfile = readurl(url, ".exchangerate.json")
-      exchangerate = float(jsonfile['rates']['INR'])
+      exchangerate = get_usdinr()
       cfg.LIVEQUOTE = liveQuoteOriginalValue
 
    return exchangerate
 
-
 if __name__ == "__main__":
-   exchangerate = get_exchangerate()
-   print("\n" + "exchangerate : " + str(exchangerate))
+   rate = get_exchangerate()
+   print "rate : " + str(rate)
 
