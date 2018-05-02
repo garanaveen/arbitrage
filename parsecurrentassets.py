0bc9c0419377f3440e5d9d7911cf3df6d47e7589
Gara Naveen <GaraNaveen@JohnDeere.com>
2018-05-02 08:20:37 -0500

    Added parsecurrentassets and exchange rate api key for fixer.io

---
+++ parsecurrentassets.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+class Asset
+   name = 'BTC'
+   holdings = 0
+
+class ExchangeAssets
+   name = 'Exchange'
+   assetList = [] #List of Asset
+   
+   
+class CurrentAssets
+   assets = [] #List of ExchangeAssets
+       
+  
