e0680c9a27d575f398effc3f35c2dd49b1f85566
Gara Naveen <GaraNaveen@JohnDeere.com>
2018-06-22 16:22:35 -0500

    fixer.io api key changed

--- exchangerateapikey.py
+++ exchangerateapikey.py
@@ -1,4 +1,4 @@
 #Create an account and replace the following url with your access_key.
-EXCHANGERATE_URL="http://data.fixer.io/api/latest?access_key=075191d834b8ceef6b09ea55dfa4b127"
+EXCHANGERATE_URL="http://data.fixer.io/api/latest?access_key=12e33fff12c8289a9e22b8de788827df"
 
 
