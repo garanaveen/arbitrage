ca3bdf521a72dbe25c2209d0056d400364078022
Ubuntu <ubuntu@ip-172-31-14-27.us-west-1.compute.internal>
2018-06-27 05:14:08 +0000

    nothing important

--- exchangerateapikey.py
+++ exchangerateapikey.py
@@ -1,4 +1,5 @@
 #Create an account and replace the following url with your access_key.
-EXCHANGERATE_URL="http://data.fixer.io/api/latest?access_key=12e33fff12c8289a9e22b8de788827df"
+EXCHANGERATE_URL="http://data.fixer.io/api/latest?access_key=075191d834b8ceef6b09ea55dfa4b127"
+
 
 
