45477a88e4f57aaa8763a209f7903933da115adf
Gara Naveen <GaraNaveen@JohnDeere.com>
2018-06-27 10:04:30 -0500

    Comparing timestamps on two files. Incomplete

--- testfile.py
+++ testfile.py
@@ -2,10 +2,30 @@
 
 #Use this file to test out generic things on python
 
-import platform
 import projectconfig as cfg
+import time
+import os
 
 if __name__ == "__main__":
-   print ("platform : ", platform.platform()) 
-   print ("ROOT_PATH : ", cfg.ROOT_PATH)
-   print ("FILE_PATH : ", cfg.FILE_NAME)
+   
+   repo_myalerts_file = cfg.ROOT_PATH + "myalerts.ini"
+   repo_myalerts_file_time = os.path.getctime(repo_myalerts_file)
+   print ("repo_myalerts_file_time : ", repo_myalerts_file_time)
+
+   home_myalerts_file = cfg.HOME_DIR + "/myalerts.ini"
+   home_myalerts_file_time = os.path.getctime(home_myalerts_file)
+   print ("home_myalerts_file_time : ", home_myalerts_file_time)
+
+   if(repo_myalerts_file > home_myalerts_file):
+      print ("repo_myalerts_file : ", repo_myalerts_file, "is the latest")
+   else:
+      print ("home_myalerts_file : ", home_myalerts_file, "is the latest")
+
+   print (type(home_myalerts_file_time))
+
+   if (1530111334.7842515 > 1530111663.7674978):
+      print ("1530111334.7842515 is larger")
+   else:
+      print ("1530111663.7674978 is larger")
+
+
